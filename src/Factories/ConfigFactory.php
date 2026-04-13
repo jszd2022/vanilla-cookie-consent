7222f482de9906fab5e4e87b141e031e2ab2d9f6
Janos-Szell David <99481700+jszd2022@users.noreply.github.com>
2026-04-13 16:32:25 +0300

    Updated factory classes with PHPDoc

--- src/Factories/ConfigFactory.php
+++ src/Factories/ConfigFactory.php
@@ -2,6 +2,9 @@
 
 namespace JSzD\VanillaCookieConsent\Factories;
 
+/*
+ * @method static \JSzD\VanillaCookieConsent\Services\ConfigService getInstance()
+ */
 class ConfigFactory extends SingletonFactory {
     protected static $class = \JSzD\VanillaCookieConsent\Services\ConfigService::class;
 }
--- src/Factories/TranslationFactory.php
+++ src/Factories/TranslationFactory.php
@@ -2,6 +2,9 @@
 
 namespace JSzD\VanillaCookieConsent\Factories;
 
+/**
+ * @method static \JSzD\VanillaCookieConsent\Services\TranslationService getInstance()
+ */
 class TranslationFactory extends SingletonFactory {
     protected static $class = \JSzD\VanillaCookieConsent\Services\TranslationService::class;
 }
