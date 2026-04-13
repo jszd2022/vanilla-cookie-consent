96df2f1cd681ed895e380dec0996c39fe26995bb
János-Széll Dávid <99481700+jszd2022@users.noreply.github.com>
2026-04-13 17:47:15 +0300

    Converted balde views to traditional php

---
+++ resources/views/info.php
@@ -0,0 +1,19 @@
+<?php foreach ($cookies->getCategories() as $category): ?>
+    <h3><?= $category->title ?></h3>
+    <table>
+        <thead>
+        <th><?= lcc_trans('cookieConsent::cookies.cookie') ?></th>
+        <th><?= lcc_trans('cookieConsent::cookies.purpose') ?></th>
+        <th><?= lcc_trans('cookieConsent::cookies.duration') ?></th>
+        </thead>
+        <tbody>
+        <?php foreach ($category->getCookies() as $cookie): ?>
+            <tr>
+                <td><?= $cookie->name ?></td>
+                <td><?= $cookie->description ?></td>
+                <td><?= lcc_minutesHumanReadable($cookie->duration) ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endforeach; ?>
